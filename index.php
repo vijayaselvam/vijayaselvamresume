f3e1602413bd638b3909f890c1e22e2adfd2bff7
vijayaselvam <43405119+vijayaselvam@users.noreply.github.com>
2024-02-03 22:37:13 +0530

    Resume_001

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport"  content="width=device-width,initial-scale=1">
+    <script src="js/jquery-3.4.1.js" type="text/javascript"></script>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="js/bootstrap.min.js" rel="stylesheet">
+</head>
+    <body>
+        </div>
+    <div class="row" id="headerrow">
+        <div class="col-sm-2">
+        <a href="index.html">HOME</a>
+        </div>
+        <div class="col-sm-2">
+        <a href="index.html">QUALIFICATION</a>
+        </div>
+         <div class="col-sm-2">
+        <a href="index.html">OBJECTIVES</a>
+        </div>
+         <div class="col-sm-2">
+        <a href="index.html">CURRICULAR</a>
+        </div>
+         <div class="col-sm-2">
+        <a href="index.html">EXTRA CURRICULAR</a>
+        </div>
+        
+        </div>
+
+<script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
